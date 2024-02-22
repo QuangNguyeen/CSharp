95fc2cce55f025e121fc423914cd8014eccd6f5d
Nguyễn Văn Quang <137971471+QuangNguyeen@users.noreply.github.com>
2024-02-23 04:20:39 +0700

    Update ScoreManagement.cs

--- HW4/Exercise1/ScoreManagement.cs
+++ HW4/Exercise1/ScoreManagement.cs
@@ -1,3 +1,4 @@
+using System.Text;
 namespace Exercise1;
 
 public class ScoreManagement
@@ -22,69 +23,74 @@ public class ScoreManagement
         Console.WriteLine("Your choice (1-5, other number to quit):");
     }
 
-    public static void AddNewScoreBoard()
+   public static void UpdateDataToFile(ScoreBoard scoreBoard,string filePath)
     {
         try
         {
-            Console.WriteLine("Enter New Subject ID: ");
-            string? idSubject = Console.ReadLine();
-            Console.WriteLine("Enter New Semester: ");
-            string? semester = Console.ReadLine();
-            StringBuilder partialName = new StringBuilder(idSubject + "_" + semester + "_rp");
-            partialName.Append(".txt");
-            DirectoryInfo hdDirectoryInWhichToSearch = new DirectoryInfo(@"/Users/macbook/Documents/C#/HW4/Exercise1/DataScore");
-            FileInfo[] filesInDir = hdDirectoryInWhichToSearch.GetFiles("*" + partialName + "*.*");
-            
-            bool check = false;
-            foreach (FileInfo foundFile in filesInDir)
+            string line = File.ReadLines(filePath).Skip(2).First();
+            string[] parts = line.Split('|');
+            double coefficient = Convert.ToDouble(parts[1]);
+            using (FileStream stream = new FileStream(filePath, FileMode.Append))
+            using (StreamWriter writer = new StreamWriter(stream))
             {
-                if (partialName.Equals(foundFile.Name))
+                if (scoreBoard.ListStudent != null)
                 {
-                    check = true;
-                    Console.WriteLine("Enter Another ID Subject and Semester. The File Already Exists.");
-                    break;
-                }
-            }
-
-            if (check == false)
-            {
-                string fullPath = Path.Combine(hdDirectoryInWhichToSearch.FullName, partialName.ToString());
-
-                if (!File.Exists(fullPath))
-                {
-                    using (File.Create(fullPath)) { }
-
-                    Console.WriteLine($"New Score Board for class '{idSubject} - {semester}' has been created successfully.");
-                    ScoreBoard scoreBoard = new ScoreBoard(fullPath);
-                    scoreBoard.Input(fullPath);
-                }
-                else
-                {
-                    Console.WriteLine($"Score Board for class '{idSubject}' already exists.");
+                    foreach (var score in scoreBoard.ListStudent)
+                    {
+                        writer.WriteLine(score.GetMarkLine(coefficient));
+                    }                    
                 }
             }
         }
         catch (Exception ex)
         {
-            Console.WriteLine($"Error Creating New Score Board: {ex.Message}");
+            Console.WriteLine($"Error in update date: {ex.Message}");
         }
     }
-public static void AddScore()
-{
-    Console.WriteLine("Enter ID Subject: ");
-    string? idSubject = Console.ReadLine();
-    Console.WriteLine("Enter Semester: ");
-    string? semester = Console.ReadLine();
-    StringBuilder partialName = new StringBuilder(idSubject + "_" + semester + "_rp");
-    partialName.Append(".txt");
-    DirectoryInfo hdDirectoryInWhichToSearch = new DirectoryInfo(@"/Users/macbook/Documents/C#/HW4/Exercise1/DataScore");
-    FileInfo[] filesInDir = hdDirectoryInWhichToSearch.GetFiles("*" + partialName + "*.*");
-    string filePath = Path.Combine(hdDirectoryInWhichToSearch.FullName, partialName.ToString());
-    bool check = false;
 
+    static string SearchScoreBoard(string id, string semester)
+    {
+        string directoryPath = "/Users/macbook/Documents/C#/HW4/Exercise1/DataScore/";
+        string partialName = $"{id}_{semester}_rp.txt";
+        DirectoryInfo hdDirectoryInWhichToSearch = new DirectoryInfo(directoryPath);
+        FileInfo[] listFiles = hdDirectoryInWhichToSearch.GetFiles("*" + partialName + "*.*");
+        string filePath = Path.Combine(hdDirectoryInWhichToSearch.FullName, partialName.ToString());
+        foreach (var file in listFiles)
+        {
+            if (partialName.Equals(file.Name))
+            {
+                return filePath;
+            }
+        }
+
+        return string.Empty;
+    }
+
+    public static void AddNewScoreBoard()
+    {
+        try
+        {
+            Console.WriteLine("Enter New Subject ID: ");
+            string? newid = Console.ReadLine() ?? "defaultID";
+            Console.WriteLine("Enter New Semester: ");
+            string? newSemester = Console.ReadLine() ?? "defaultSemester";
+            // check exist
+            string filePath = SearchScoreBoard(newid, newSemester);
+            if (filePath == string.Empty)
+            {
+                filePath = $"/Users/macbook/Documents/C#/HW4/Exercise1/DataScore/{newid}_{newSemester}_rp.txt";
+            }
+            ScoreBoard scoreBoard = new ScoreBoard(filePath);
+            scoreBoard.Input(filePath);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error in create new Score Board: {ex.Message}");
+        }
+    } 
     static int CountLinesFromLine(string filePath, int startLine)
     {
-        if (!File.Exists(filePath))
+        if (filePath == string.Empty)
         {
             throw new FileNotFoundException("File not found.", filePath);
         }
@@ -92,47 +98,91 @@ public static void AddScore()
         int count = File.ReadLines(filePath).Skip(startLine - 1).Count();
         return count;
     }
-
-    foreach (FileInfo foundFile in filesInDir)
-    {
-        if (partialName.ToString().Equals(foundFile.Name))
-        {
-            check = true;
-            int numberOfLines = CountLinesFromLine(filePath, 7);
-            string line = File.ReadLines(filePath).Skip(4).First();
-            string[] parts = line.Split('|');
-            var quantity = Convert.ToInt32(parts[1]);
-            line = File.ReadLines(filePath).Skip(2).First();
-            parts = line.Split('|');
-            var processCoefficient = Convert.ToDouble(parts[1]);
-            Console.WriteLine($"There are {quantity - numberOfLines - 1} students who do not have scores yet.");
-            Console.WriteLine("Enter the number student to add Score: ");
-            var number = Convert.ToInt32(Console.ReadLine());
-            if (numberOfLines == 0 && number < quantity || numberOfLines != 0 && number < quantity - numberOfLines)
-            {
-                using (var stream = new FileStream(filePath, FileMode.Append))
-                using (var writer = new StreamWriter(stream))
-                {
-                    for (var i = 0; i < number; i++)
-                    {
-                        StudentScore student = new StudentScore();
-                        student.Input();
-                        writer.WriteLine(student.GetMarkLine(processCoefficient));
-                    }
-                } 
-            }
-            else
-            {
-                Console.WriteLine("The class is full or there are not enough seats.");
-            }
-        }
-    }
-    if (!check)
-    {
-        Console.WriteLine("File Not Exist.");
-    }
+ public static void AddStudentScore()
+ {
+     try
+     {
+         Console.WriteLine("Enter Subject ID: ");
+         string idSubject = Console.ReadLine() ?? "defaultID";
+         Console.WriteLine("Enter Semester: ");
+         string semester = Console.ReadLine() ?? "defaultSemester";
+         string filePath = SearchScoreBoard(idSubject, semester);
+         if (!string.IsNullOrEmpty(filePath))
+         {
+             ScoreBoard scoreBoard = new ScoreBoard(filePath);
+             scoreBoard.ListStudent = new List<StudentScore>();
+             int numberOfLines = CountLinesFromLine(filePath, 6);
+             int quantity = scoreBoard.QuantityStudent;
+             Console.WriteLine($"Can add {quantity - numberOfLines} Student Score.");
+             Console.WriteLine("Enter the number student to add Score: ");
+             int number = Convert.ToInt32(Console.ReadLine());
+             
+             // check quantity student in file
+             if (numberOfLines == 0 && number < quantity || numberOfLines != 0 && number < quantity - numberOfLines)
+             {
+                 {
+                     for (var i = 0; i < number; i++)
+                     {
+                         StudentScore student = new StudentScore();
+                         student.Input();
+                         if (scoreBoard.ListStudent != null)
+                         {
+                             scoreBoard.ListStudent.Add(student);
+                         }
+                     }
+                     UpdateDataToFile(scoreBoard,filePath);
+                 }
+             }
+             else
+             {
+                 Console.WriteLine("The class is full or there are not enough seats.");
+             }
+         }
+         else
+         {
+             Console.WriteLine("File not found. Unable to add scores.");
+         }
+     }
+     catch (Exception ex)
+     {
+         Console.WriteLine($"Error in Add Score Student: {ex.Message}");
+     }
 }
-    public static void Main(string[] args)
+
+ public static void RemoveStudentScore()
+ {
+     try
+     {
+         Console.WriteLine("Enter Subject ID: ");
+         string idSubject = Console.ReadLine() ?? "defaultID";
+         Console.WriteLine("Enter Semester: ");
+         string semester = Console.ReadLine() ?? "defaultSemester";
+         string filePath = SearchScoreBoard(idSubject, semester);
+         if (filePath != string.Empty)
+         {
+             ScoreBoard scoreBoard = new ScoreBoard(filePath);
+             Console.WriteLine("Enter Student ID: ");
+             string? id = Console.ReadLine();
+             if (scoreBoard.ListStudent != null)
+             {
+                 int quantity = scoreBoard.ListStudent.Count;
+                 for (int i = 0; i < quantity; i++)
+                 {
+                     if (id != null && id.Equals(scoreBoard.ListStudent[i].Id))
+                     {
+                         scoreBoard.ListStudent.RemoveAt(i);
+                     }
+                 }
+             }
+             UpdateDataToFile(scoreBoard,filePath);
+         }
+     }
+     catch (Exception ex)
+     {
+         Console.WriteLine($"Error in remove Student Score: {ex.Message}");
+     }
+ }
+public static void Main(string[] args)
     {
         int choice;
         do
@@ -148,11 +198,12 @@ public static void AddScore()
                 }
                 case Option.AddScore:
                 {
-                    AddScore();
+                    AddStudentScore();
                     break;
                 }
                 case Option.RemoveScore:
                 {
+                    RemoveStudentScore();
                     break;
                 }
                 case Option.SearchScore:
